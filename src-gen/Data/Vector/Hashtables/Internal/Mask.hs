2c6d699d7cbf3afbc91f248731f09d240ce711c2
Andrey Prokopenko <persiantiger@yandex.ru>
2021-08-21 11:20:09 +0200

    Add arch-dependent (i386 and rest) int mask and resolve it via cabal

---
+++ src-gen/Data/Vector/Hashtables/Internal/Mask.hs
@@ -0,0 +1,4 @@
+module Data.Vector.Hashtables.Internal.Mask where
+
+mask = 0x7FFFFFFFFFFFFFFF :: Int
+{-# INLINE mask #-}
---
+++ src-i386/Data/Vector/Hashtables/Internal/Mask.hs
@@ -0,0 +1,4 @@
+module Data.Vector.Hashtables.Internal.Mask where
+
+mask = 0x7FFFFFFF :: Int
+{-# INLINE mask #-}
